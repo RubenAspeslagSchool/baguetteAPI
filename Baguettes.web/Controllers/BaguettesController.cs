d88b95ca395b4c9c15545e53480f2e7b429b3453
RubenAspeslagSchool <ruben.aspeslag@student.howest.be>
2025-03-27 14:02:11 +0100

    Test

---
+++ Baguettes.web/Controllers/BaguettesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baguettes.web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaguettesController : ControllerBase
+    {
+        [HttpGet]
+        public GetBaguette()
+        {
+
+        }
+    }
+}
