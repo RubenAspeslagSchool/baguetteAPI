e2fc6c9d566787d5a5df4499f61755bacd6b9d99
Thibo Vercruysse <thibo.vercruysse@student.howest.be>
2025-03-27 14:20:11 +0100

    Add dbContext

---
+++ Baguettes.Core/Data/BaguetteDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Baguettes.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baguettes.Core.Data
+{
+    public class BaguetteDbContext : DbContext
+    {
+        public BaguetteDbContext(DbContextOptions<BaguetteDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Baguette> Baguettes { get; set; }
+    }
+}
