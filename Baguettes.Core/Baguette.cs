a34aa5de23009879bba33bcaac6dbe9695e29c80
Thibo Vercruysse <thibo.vercruysse@student.howest.be>
2025-03-27 14:13:28 +0100

    Add Core

---
+++ Baguettes.Core/Baguette.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baguettes.Core
+{
+    public class Baguette
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
